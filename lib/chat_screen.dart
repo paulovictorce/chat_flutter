778c8768e97a1d207339c2e17ef8b8a942156b0d
Paulo Victor <bd.paulovictor@gmail.com>
2020-10-20 12:30:40 -0300

    Implementando o login com o Google

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -2,32 +2,81 @@ import 'dart:io';
 
 import 'package:chat/text_composer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ChatScreen extends StatefulWidget {
-
-   @override
+  @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  FirebaseUser _currentUser;
 
-  void _sendMessage({String text, File imgFile}) async{
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.onAuthStateChanged.listen((user) {
+      _currentUser = user;
+    });
+  }
 
-    Map<String, dynamic> data = {};
+  Future<FirebaseUser> _getUser() async {
+    if(_currentUser != null) return _currentUser;
+    try {
+      final GoogleSignInAccount googleSignInAccount =
+          await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
 
-    if(imgFile != null) {
-      StorageUploadTask storageUploadTask = FirebaseStorage.instance.ref().child(
-        DateTime.now().millisecondsSinceEpoch.toString()
-      ).putFile(imgFile);
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
 
-      StorageTaskSnapshot storageTaskSnapshot =  await storageUploadTask.onComplete;
+      final AuthResult authResult =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      final FirebaseUser user = authResult.user;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  void _sendMessage({String text, File imgFile}) async {
+    final FirebaseUser user = await _getUser();
+
+    if(user == null) {
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text('Não foi possível realizar o login. Tente novamente!'),
+          backgroundColor: Colors.red,
+        )
+      );
+    }
+
+    Map<String, dynamic> data = {
+      "uid": user.uid,
+      "senderName": user.displayName,
+      "senderPhotoUrl": user.photoUrl,
+    };
+
+    if (imgFile != null) {
+      StorageUploadTask storageUploadTask = FirebaseStorage.instance
+          .ref()
+          .child(DateTime.now().millisecondsSinceEpoch.toString())
+          .putFile(imgFile);
+
+      StorageTaskSnapshot storageTaskSnapshot =
+          await storageUploadTask.onComplete;
       String url = await storageTaskSnapshot.ref.getDownloadURL();
       data['imgUrl'] = url;
     }
 
-    if(text != null) data['text'] = text;
+    if (text != null) data['text'] = text;
 
     Firestore.instance.collection('messages').add(data);
   }
@@ -35,6 +84,7 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text('Olá'),
         elevation: 0,
@@ -45,23 +95,23 @@ class _ChatScreenState extends State<ChatScreen> {
             child: StreamBuilder<QuerySnapshot>(
               stream: Firestore.instance.collection('messages').snapshots(),
               builder: (context, snapshot) {
-                switch(snapshot.connectionState){
+                switch (snapshot.connectionState) {
                   case ConnectionState.none:
                   case ConnectionState.waiting:
                     return Center(
                       child: CircularProgressIndicator(),
                     );
                   default:
-                    List<DocumentSnapshot> documents = snapshot.data.documents.reversed.toList();
+                    List<DocumentSnapshot> documents =
+                        snapshot.data.documents.reversed.toList();
                     return ListView.builder(
                         itemCount: documents.length,
                         reverse: true,
-                        itemBuilder: (context, index){
+                        itemBuilder: (context, index) {
                           return ListTile(
                             title: Text(documents[index].data['text']),
                           );
-                        }
-                    );
+                        });
                 }
               },
             ),
